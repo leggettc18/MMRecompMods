3ddfebb49f0057ecf09633ef43c67419c16623ed
Christopher Leggett <chris@leggett.dev>
2026-02-07 15:03:43 -0500

    Scroll text 10x faster

--- src/fast_text.c
+++ src/fast_text.c
@@ -3,14 +3,16 @@
 #include "recomputils.h"
 #include "recompconfig.h"
 
-MessageContext* msgCtx;
-
-RECOMP_HOOK("Message_DrawTextNES") void draw_text_nes_hook(PlayState* play, Gfx** gfxP, u16 textDrawPos) {
-    msgCtx = &play->msgCtx;
+RECOMP_HOOK("Message_Update") void message_update_hook(PlayState* play) {
+    MessageContext* msgCtx = &play->msgCtx;
+    if (msgCtx->msgMode == MSGMODE_TEXT_DISPLAYING) {
+        msgCtx->textDrawPos = MIN(msgCtx->textDrawPos + 10, msgCtx->decodedTextLen+1);
+    }
 }
 
-RECOMP_HOOK_RETURN("Message_DrawTextNES") void draw_text_nes_return_hook() {
-    msgCtx->textDelay = msgCtx->textDelay / 2;
-}
+// RECOMP_HOOK_RETURN("Message_DrawText") void draw_text_return_hook() {
+//     msgCtx->textDelayTimer = MAX(msgCtx->textDelayTimer - 3, 0);
+//     msgCtx->textDrawPos = MIN(msgCtx->textDrawPos + 2, msgCtx->decodedTextLen);
+// }
 
 
