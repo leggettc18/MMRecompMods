02f0e1bfcd0fbfffae5681b483b2c24eb9d1d936
Christopher Leggett <chris@leggett.dev>
2026-02-07 15:32:32 -0500

    Add options for text scrolling multiplier

--- src/fast_text.c
+++ src/fast_text.c
@@ -3,10 +3,32 @@
 #include "recomputils.h"
 #include "recompconfig.h"
 
+typedef enum {
+    TEXT_SPEED_2X,
+    TEXT_SPEED_4X,
+    TEXT_SPEED_8X,
+    TEXT_SPEED_16X,
+} TextSpeed;
+
 RECOMP_HOOK("Message_Update") void message_update_hook(PlayState* play) {
     MessageContext* msgCtx = &play->msgCtx;
+    unsigned int textSpeedMod = 0;
+    switch(recomp_get_config_u32("text_scroll_mult")) {
+        case TEXT_SPEED_2X:
+            textSpeedMod = 1;
+            break;
+        case TEXT_SPEED_4X:
+            textSpeedMod = 3;
+            break;
+        case TEXT_SPEED_8X:
+            textSpeedMod = 7;
+            break;
+        case TEXT_SPEED_16X:
+            textSpeedMod = 15;
+            break;
+    }
     if (msgCtx->msgMode == MSGMODE_TEXT_DISPLAYING) {
-        msgCtx->textDrawPos = MIN(msgCtx->textDrawPos + 10, msgCtx->decodedTextLen+1);
+        msgCtx->textDrawPos = MIN(msgCtx->textDrawPos + textSpeedMod, msgCtx->decodedTextLen+1);
     }
 }
 
