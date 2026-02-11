2301f5bb9eeb875980452f70e48283771d3e6c20
Christopher Leggett <chris@leggett.dev>
2026-02-10 20:31:12 -0500

    Add ability to auto advance text boxes by holding B

--- packages/FastText/src/fast_text.c
+++ packages/FastText/src/fast_text.c
@@ -3,6 +3,8 @@
 #include "recomputils.h"
 #include "recompconfig.h"
 
+s32 Message_BombersNotebookProcessEventQueue(PlayState* play);
+
 typedef enum {
     TEXT_SPEED_2X,
     TEXT_SPEED_4X,
@@ -10,8 +12,15 @@ typedef enum {
     TEXT_SPEED_16X,
 } TextSpeed;
 
+typedef enum {
+    AUTO_ADV_ON,
+    AUTO_ADV_OFF,
+} AutoAdvanceState;
+
 RECOMP_HOOK("Message_Update") void message_update_hook(PlayState* play) {
     MessageContext* msgCtx = &play->msgCtx;
+    Input* input = CONTROLLER1(&play->state);
+    PauseContext* pauseCtx = &play->pauseCtx;
     unsigned int textSpeedMod = 0;
     switch(recomp_get_config_u32("text_scroll_mult")) {
         case TEXT_SPEED_2X:
@@ -27,14 +36,49 @@ RECOMP_HOOK("Message_Update") void message_update_hook(PlayState* play) {
             textSpeedMod = 15;
             break;
     }
+
     if (msgCtx->msgMode == MSGMODE_TEXT_DISPLAYING) {
         msgCtx->textDrawPos = MIN(msgCtx->textDrawPos + textSpeedMod, msgCtx->decodedTextLen+1);
+        if (CHECK_BTN_ALL(input->cur.button, BTN_B)) {
+            msgCtx->textDrawPos = msgCtx->decodedTextLen+1;
+        }
     }
 }
 
-// RECOMP_HOOK_RETURN("Message_DrawText") void draw_text_return_hook() {
-//     msgCtx->textDelayTimer = MAX(msgCtx->textDelayTimer - 3, 0);
-//     msgCtx->textDrawPos = MIN(msgCtx->textDrawPos + 2, msgCtx->decodedTextLen);
-// }
+RECOMP_PATCH bool Message_ShouldAdvance(PlayState* play) {
+    MessageContext* msgCtx = &play->msgCtx;
+    Input* controller = CONTROLLER1(&play->state);
+
+    if ((msgCtx->textboxEndType == TEXTBOX_ENDTYPE_TWO_CHOICE) ||
+        (msgCtx->textboxEndType == TEXTBOX_ENDTYPE_THREE_CHOICE)) {
+        if (CHECK_BTN_ALL(controller->press.button, BTN_A)) {
+            Audio_PlaySfx(NA_SE_SY_MESSAGE_PASS);
+        }
+        return CHECK_BTN_ALL(controller->press.button, BTN_A);
+    } else {
+        if (CHECK_BTN_ALL(controller->press.button, BTN_A) || CHECK_BTN_ALL(controller->press.button, BTN_B) ||
+            CHECK_BTN_ALL(controller->press.button, BTN_CUP) || 
+            (recomp_get_config_u32("auto_advance_text") == AUTO_ADV_ON && CHECK_BTN_ALL(controller->cur.button, BTN_B))) {
+            Audio_PlaySfx(NA_SE_SY_MESSAGE_PASS);
+        }
+        return CHECK_BTN_ALL(controller->press.button, BTN_A) || CHECK_BTN_ALL(controller->press.button, BTN_B) ||
+               CHECK_BTN_ALL(controller->press.button, BTN_CUP) ||  
+                (recomp_get_config_u32("auto_advance_text") == AUTO_ADV_ON && CHECK_BTN_ALL(controller->cur.button, BTN_B));
+    }
+}
+
+RECOMP_PATCH bool Message_ShouldAdvanceSilent(PlayState* play) {
+    MessageContext* msgCtx = &play->msgCtx;
+    Input* controller = CONTROLLER1(&play->state);
+
+    if ((msgCtx->textboxEndType == TEXTBOX_ENDTYPE_TWO_CHOICE) ||
+        (msgCtx->textboxEndType == TEXTBOX_ENDTYPE_THREE_CHOICE)) {
+        return CHECK_BTN_ALL(controller->press.button, BTN_A);
+    } else {
+        return CHECK_BTN_ALL(controller->press.button, BTN_A) || CHECK_BTN_ALL(controller->press.button, BTN_B) ||
+               CHECK_BTN_ALL(controller->press.button, BTN_CUP) ||  
+                (recomp_get_config_u32("auto_advance_text") == AUTO_ADV_ON && CHECK_BTN_ALL(controller->cur.button, BTN_B));
+    }
+}
 
 
